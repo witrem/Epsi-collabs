2561dedbd0d3a7635c9047cd6b61818eccbe713e
Unknown <alexislegeay@hotmail.fr>
2018-03-19 14:23:59 +0100

    Update Modal

--- Modals/m_profil.php
+++ Modals/m_profil.php
@@ -5,6 +5,7 @@
         <meta charset="UTF-8">
     </head>
     <body>
+        
         <div id="modalprofile" class="modal">
             <div class="modal-content">
                 <h4 class="titreprofil">Profil</h4>
@@ -12,13 +13,57 @@
                   <div class="col s12">
                 <img src="Images/profil.jpg" alt="" class="circle responsive-img" width="15%">
                 
-                
-                
+       
+
+
+      <table class="infoprofil">
+        <thead>
+          <tr>
+              <th>Infos</th>
+              <th>Données</th>            
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Nom</td>
+            <td>Legeay</td>
+           
+          </tr>
+          <tr>
+            <td>Prénom</td>
+            <td>Alexis</td>
+            
+          </tr>
+          <tr>
+            <td>Campus</td>
+            <td>Nantes</td>
+            
+          </tr>
+          <tr>
+            <td>Niveau</td>
+            <td>B1</td>
+            
+          </tr>
+          <tr>
+            <td>Email</td>
+            <td>alexis.legeay@epsi.fr</td> 
+          </tr>
+          <tr>
+            <td>Social</td>
+            <td>https://twitter.com/LegeayAlexis</td> 
+          </tr>
+          
+        </tbody>
+      </table>
+            
+
+
                     
                </div>
             </div>
             <div class="modal-footer">
-                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Rester</a>
+                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Fermer</a>
             </div>
         </div>
     </body>
