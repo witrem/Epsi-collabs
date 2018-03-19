1ee0738e57cb310fc5338d78df11d7bc3965dd36
YumYumi <romain.boudot@epsi.fr>
2018-03-19 12:59:39 +0100

    cmpétence + html>php

---
+++ index.php
@@ -0,0 +1,45 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, userscalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Epsi Collabs</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper bg-epsi">
+            <a href="#" class="brand-logo ml-1">Epsi Collabs</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Déconnexion</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <input type="text" id="search" placeholder="Compétences, artices">
+
+    <section id="competences" class="txt-center txt-light card border">
+        <table>
+            <thead>
+                <tr>
+                    <th id="comp-title" class="txt-center">Compétences</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="clickable">PHP</td>
+                </tr>
+                <tr>
+                    <td class="clickable">Javascript</td>
+                </tr>
+                <tr>
+                    <td class="clickable">C++</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+
+</body>
+</html>
