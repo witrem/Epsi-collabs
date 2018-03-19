d7f5879ae5175081e3fdfa912ed9691f3f59cfc3
Unknown <alexislegeay@hotmail.fr>
2018-03-19 13:47:54 +0100

    Ajout Modal Profil

---
+++ Modals/m_profil.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div id="modalprofile" class="modal">
+            <div class="modal-content">
+                <h4 class="titreprofil">Profil</h4>
+             <div class="row alignement">
+                  <div class="col s12">
+                <img src="Images/profil.jpg" alt="" class="circle responsive-img" width="15%">
+                
+                
+                
+                    
+               </div>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Rester</a>
+            </div>
+        </div>
+    </body>
+</html>
--- index.php
+++ index.php
@@ -5,14 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
     <link rel="stylesheet" href="main.css">
+     <link rel="stylesheet" href="modal.css">
+     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
     <title>Epsi Collabs</title>
+   
+  
+  
 </head>
 <body>
+     <script>
+     $(document).ready(function(){
+   
+      $('.modal-trigger').leanModal();
+  });
+  </script>   
     <nav>
         <div class="nav-wrapper bg-epsi">
             <a href="#" class="brand-logo ml-1">Epsi Collabs</a>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="#">Profile</a></li>
+                <li><a class="modal-trigger" href="#modalprofile">Profile</a></li>
                 <li><a href="#">Déconnexion</a></li>
             </ul>
         </div>
@@ -40,6 +52,6 @@
             </tbody>
         </table>
     </section>
-
+<?php include('Modals/m_profil.php'); ?>
 </body>
 </html>
