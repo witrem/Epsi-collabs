e1d0d0a89a3d21de4a24d43841f94b45ea7547d9
Unknown <romain.boudot@epsi.fr>
2018-03-21 16:48:13 +0100

    responsive cards

--- Js/calendar.js
+++ Js/calendar.js
@@ -1,37 +1,3 @@
-$(document).ready(function(){
-
-    campus = $("#campus_select").val();
-    
-    comp = $("#comp_select").val();
-
-    console.log(campus + ' . ' + comp)
-
-    $('#calendar').fullCalendar({
-        height: 'parent',
-        columnHeader: true,
-        nowIndicator: true,
-        editable: false,
-        eventSources: [
-            {
-                url: 'http://localhost:88/Modules/EventFeeder.php',
-                type: 'POST',
-                data: {
-                    comp: comp,
-                    campus: campus
-                },
-                error: function() {
-                    alert('there was an error while fetching events!');
-                },
-                color: 'blue',
-                textColor: 'white'
-            }
-          ]
-    });
-
-    $('#calendar').fullCalendar('changeView', 'agendaWeek');
-
-});
-
 function update() {
     $('#calendar').fullCalendar('removeEventSources');
     campus = $("#campus_select").val();
@@ -52,3 +18,45 @@ function update() {
         }
     )
 }
+
+$(document).ready(function(){
+
+    campus = $("#campus_select").val();
+    
+    comp = $("#comp_select").val();
+
+    console.log(campus + ' . ' + comp)
+
+    $('#calendar').fullCalendar({
+        height: 'parent',
+        columnHeader: true,
+        nowIndicator: true,
+        editable: false,
+        header: {
+            left:   '',
+            center: 'title',
+            right:  'today prev,next',
+        },
+        eventSources: [
+            {
+                url: 'http://localhost:88/Modules/EventFeeder.php',
+                type: 'POST',
+                data: {
+                    comp: comp,
+                    campus: campus
+                },
+                error: function() {
+                    alert('there was an error while fetching events!');
+                },
+                color: 'blue',
+                textColor: 'white'
+            }
+          ]
+    });
+
+    $('#calendar').fullCalendar('changeView', 'agendaWeek');
+
+    $('.fc-left').html('<div class="clickable btn_menu_calendar_resp"><i class="material-icons">menu</i></div>')
+
+});
+
--- User.php
+++ User.php
@@ -34,124 +34,124 @@ if (!is_login()) {
 </head>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/Includes/nav.php" ?>
 <body>
-        <?php
-           
-            if (isset($_GET['id'])) {
-                $id = $_GET['id'];
-            } else {
-                $id = $_SESSION['user']['id'];
-            }
-            $result = User::get_user_info($id);
+    <?php
+        
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+        } else {
+            $id = $_SESSION['user']['id'];
+        }
+        $result = User::get_user_info($id);
 
-        ?>
+    ?>
 
-        <div id="login-wrapper" class="card mh-auto profiluser card-responsive"> 
-            <div class="row alignement">
+    <div id="login-wrapper" class="card mh-auto profiluser card-responsive"> 
+        <div class="row alignement">
 
-                <div class="col s12 p-no">
-                    <nav class="nav-extended bg-epsi3">
+            <div class="col s12 p-no">
+                <nav class="nav-extended bg-epsi3">
 
-                        <div class="nav-content">
-                            <ul class="tabs tabs-transparent">
-                                <li class="tab"><a href="#test1">Profil</a></li>
-                                <li class="tab"><a href="#test2">Calendrier</a></li>
-                                <li class="tab"><a href="#test3">Contact</a></li>
+                    <div class="nav-content">
+                        <ul class="tabs tabs-transparent">
+                            <li class="tab"><a href="#test1">Profil</a></li>
+                            <li class="tab"><a href="#test2">Calendrier</a></li>
+                            <li class="tab"><a href="#test3">Contact</a></li>
 
-                            </ul>
-                        </div>
-                    </nav>
-                    <div id="test1" class="col s12">
-
-                        <img src="/Assets/Avatar/<?php echo $result[0]['Photo']; ?>" alt="" class="circle responsive-img photo" width="15%">
-                        <table class="infoprofil">
-
-                            <thead>
-                                <tr>
-                                    <th>Infos</th>
-                                    <th>Données</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr>
-                                    <td>Nom</td>
-                                    <td><?php echo $result['Nom']; ?></td>
-
-                                </tr>
-                                <tr>
-                                    <td>Prénom</td>
-                                    <td><?php echo $result['Prenom']; ?></td>
-
-                                </tr>
-                                <tr>
-                                    <td>Description</td>
-                                    <td><?php echo $result['Description']; ?></td>
-
-                                </tr>
-                                <tr>
-                                    <td>Campus</td>
-                                    <td><?php echo $result['Nom_Campus']; ?></td>
-
-                                </tr>
-                                <tr>
-                                    <td>Niveau</td>
-                                    <td><?php echo $result['Niveau']; ?></td>
-
-                                </tr>
-                                <tr>
-                                    <td>Email</td>
-                                    <td><?php echo $result['Email']; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Social 1</td>
-                                    <td><?php echo $result['Social1']; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Social 2</td>
-                                    <td><?php echo $result['Social2']; ?></td>
-                                </tr>
-                                <tr>
-                                    <td>Social 3</td>
-                                    <td><?php echo $result['Social3']; ?></td>
-                                </tr>
-
-                            </tbody>
-                        </table> 
-                        <div class="Talents">
-                            <p class="comptext">Super Pouvoir</p>
-
-                            <?php
-
-                                $result = User::get_comp_propose($id);
-                                foreach ($result as $ligne) {
-                                    echo "<div class='chip'>",$ligne['Nom'],"</div>";
-                                }
-                            
-                            ?>
-                          
-                        </div>
+                        </ul>
                     </div>
-                    <div id="test2" class="col s12">SOON</div>
-                    <div id="test3" class="col s12">
-                        <div class="row">
-                            <form class="col s12">
-                                <div class="row">
-                                    <div class="input-field col s12 messagerie">
-                                        <i class="material-icons prefix">mode_edit</i>
-                                        <textarea id="icon_prefix2" class="materialize-textarea "></textarea>
-                                        <label for="icon_prefix2">Ton message</label>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                </nav>
+                <div id="test1" class="col s12">
 
-                        <a class="btn-floating btn-large modal-trigger bg-epsi5 modalbutton modal-action modal-close" href="#"><i class="material-icons">email</i></a>
+                    <img src="/Assets/Avatar/<?php echo $result[0]['Photo']; ?>" alt="" class="circle responsive-img photo" width="15%">
+                    <table class="infoprofil">
 
+                        <thead>
+                            <tr>
+                                <th>Infos</th>
+                                <th>Données</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr>
+                                <td>Nom</td>
+                                <td><?php echo $result['Nom']; ?></td>
+
+                            </tr>
+                            <tr>
+                                <td>Prénom</td>
+                                <td><?php echo $result['Prenom']; ?></td>
+
+                            </tr>
+                            <tr>
+                                <td>Description</td>
+                                <td><?php echo $result['Description']; ?></td>
+
+                            </tr>
+                            <tr>
+                                <td>Campus</td>
+                                <td><?php echo $result['Nom_Campus']; ?></td>
+
+                            </tr>
+                            <tr>
+                                <td>Niveau</td>
+                                <td><?php echo $result['Niveau']; ?></td>
+
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td><?php echo $result['Email']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Social 1</td>
+                                <td><?php echo $result['Social1']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Social 2</td>
+                                <td><?php echo $result['Social2']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Social 3</td>
+                                <td><?php echo $result['Social3']; ?></td>
+                            </tr>
+
+                        </tbody>
+                    </table> 
+                    <div class="Talents">
+                        <p class="comptext">Super Pouvoir</p>
+
+                        <?php
+
+                            $result = User::get_comp_propose($id);
+                            foreach ($result as $ligne) {
+                                echo "<div class='chip'>",$ligne['Nom'],"</div>";
+                            }
+                        
+                        ?>
+                        
                     </div>
                 </div>
+                <div id="test2" class="col s12">SOON</div>
+                <div id="test3" class="col s12">
+                    <div class="row">
+                        <form class="col s12">
+                            <div class="row">
+                                <div class="input-field col s12 messagerie">
+                                    <i class="material-icons prefix">mode_edit</i>
+                                    <textarea id="icon_prefix2" class="materialize-textarea "></textarea>
+                                    <label for="icon_prefix2">Ton message</label>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
 
+                    <a class="btn-floating btn-large modal-trigger bg-epsi5 modalbutton modal-action modal-close" href="#"><i class="material-icons">email</i></a>
+
+                </div>
             </div>
-        
+
+        </div>
+    
       
 </body>
 
